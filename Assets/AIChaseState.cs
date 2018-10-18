85fd52e9f55ed1ed4a7fbba8c9b3649de87efd7e
Sergi Ledesma <sergi.dev@gmail.com>
2018-10-18 14:28:32 +0200

    Initial project commit, including a character with a sphere collider for detection, a state machine with Idle, Chase and Attack and the Ethan to trigger the chase state walking into the collider.

---
+++ Assets/AIChaseState.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIChaseState : StateMachineBehaviour
+{
+
+    Zombie zombie;
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        Debug.Log("Chasing...");
+        if (zombie == null)
+        {
+            zombie = animator.transform.gameObject.GetComponent<Zombie>();
+        }
+
+        zombie.nav.speed = zombie.chaseSpeed;
+        zombie.nav.isStopped = false;
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (zombie.targetGO)
+        {
+            zombie.nav.destination = zombie.targetGO.transform.position;
+            zombie.PlaceMarker(zombie.targetGO.transform.position);
+
+            if (Vector3.Distance(zombie.transform.position, zombie.targetGO.transform.position) < zombie.attackRange)
+            {
+                zombie.animator.SetTrigger("PlayerInAttackRange");
+            }
+        }
+        else
+        {
+            zombie.animator.SetBool("PlayerInChaseRange", false);
+        }
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+    //
+    //}
+
+    // OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+    //
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK(). Code that sets up animation IK (inverse kinematics) should be implemented here.
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+    //
+    //}
+}
---
+++ Assets/Zombie.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zombie : MonoBehaviour {
+
+    public Animator animator;
+    public float chaseSpeed = 2;
+    public float attackRange = 2;
+
+    public GameObject targetGO;
+
+    [SerializeField]
+    private GameObject targetMarker;
+
+    [HideInInspector]
+    public UnityEngine.AI.NavMeshAgent nav;
+    
+    void Start ()
+    {
+        targetGO = null;
+        animator = GetComponent<Animator>();
+
+        nav = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        nav.speed = chaseSpeed;
+    }
+	
+	void Update () {
+		
+	}
+
+    public void PlaceMarker(Vector3 position)
+    {
+        GameObject marker = Instantiate(targetMarker, position, Quaternion.identity) as GameObject;
+        StartCoroutine(DestroyMarker(marker));        
+    }
+
+    IEnumerator DestroyMarker(GameObject marker)
+    {
+        yield return new WaitForSeconds(0.3f);
+        Destroy(marker);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("PLAYER DETECTED");
+            targetGO = other.gameObject;
+        }
+    }
+
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("PLAYER LOST");
+            targetGO = null;
+        }
+    }
+
+}
